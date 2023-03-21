9c2d0c6060bfe8cc5041994c005a9d7437d1a9fe
lorossi <mail@lorenzoros.si>
2023-03-21 12:44:12 +0100

    changed code to something more representative

--- js/main.js
+++ js/main.js
@@ -12,28 +12,15 @@ const main = () => {
   model.setView(view);
 
   controller.setCode([
-    ".globl",
-    "a: .word 1 2 3 4 5",
-    "b: .word 6 7 8 9 10",
-    "out: .space 5",
     ".text",
-    "la $t0, a",
-    "la $t1, b",
-    "la $t2, out",
-    "li $t3, 5",
-    "li $t4, 0",
-    "loop:",
-    "beq $t4, $t3, end",
-    "lw $t5, 0($t0)",
-    "lw $t6, 0($t1)",
-    "add $t7, $t5, $t6",
-    "sw $t7, 0($t2)",
-    "addi $t0, $t0, 4",
-    "addi $t1, $t1, 4",
-    "addi $t2, $t2, 4",
-    "addi $t4, $t4, 1",
-    "jump loop",
-    "end:",
+    "LI $t0, 1000",
+    "LOOP:",
+    "LI $t1, 100",
+    "LOOP2:",
+    "SUBI $t1, $t1, 1",
+    "BNE $t1, $zero, LOOP2",
+    "SUBI $t0, $t0, 1",
+    "BNE $t0, $zero, LOOP",
   ]);
 };
 
